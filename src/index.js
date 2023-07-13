c5cfe1c225013c11292eb0e72a3781cfa18ef74b
Ratnesh patel <ratneshpatel8815@gmail.com>
2023-07-13 09:30:40 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as ReactDOMClient from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
+
+
+reportWebVitals();
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Products from '../components/Products';
+
+const Home = () => {
+    return (
+        <div>
+           
+            <section>
+                <h3>Products</h3>
+                <Products />
+            </section>
+        </div>
+    );
+};
+
+export default Home;
